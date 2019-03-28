2720440c6feb6fb2a2ba25c07ac1a421246f3f15
Timothy McFadden <tim@timandjamie.com>
2019-03-27 18:19:46 -0600

    adding conftest.py for configuring pytest

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+"""
+Configure the environment for testing.
+"""
+import os
+import sys
+import django
+
+# TODO: Remove this if we fix the folder structure.  # pylint: disable=fixme
+# We only need this because the base app folder (where `manage.py` is) is not
+# where we run the tests.
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "badging.badging.settings")
+
+
+def pytest_configure():
+    django.setup()
