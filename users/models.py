9a7e2340205746ebb8647e657f5770c338dffc63
kyrogon <aaron.kyle.rogers@gmail.com>
2019-05-28 19:40:40 -0600

    Made User/Badge relationship manytomany

--- users/models.py
+++ users/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Person(models.Model):
     name = models.CharField(max_length=100)
+    badge_set = models.ManyToManyField('Badge', related_name='user_set')
 
     def __repr__(self) -> str:
         return f"Person[{self.name}]"
@@ -12,14 +13,14 @@ class Person(models.Model):
 
 
 class Badge(models.Model):
-    name = models.CharField(max_length=200)
-    person = models.ForeignKey(Person, on_delete=models.CASCADE)
-    presenter = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
+    # person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    presenter = models.CharField(max_length=200, unique=True)
 
     def __repr__(self) -> str:
         return (
-            f"Badge[name={self.name}, person={self.person}, "
-            f"presenter={self.presenter}]"
+            f"Badge[name={self.name}, presenter={self.presenter}]"
+            # f"presenter={self.presenter}]"
         )
 
     def __str__(self) -> str:
