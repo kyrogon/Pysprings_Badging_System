71af96bd6c2da9714ff47956b262d50297d4936c
kyrogon <aaron.kyle.rogers@gmail.com>
2019-05-30 16:19:53 -0600

    Renamed views for better clarity

--- users/urls.py
+++ users/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = "users"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:person_id>/", views.detail, name="detail"),
-    path("<int:person_id>/badge/", views.badge, name="badge"),
-    path("<int:person_id>/add_badge/", views.add_badge, name="add_badge"),
-    path("add_person", views.add_person, name="add_person"),
+    path("user/new/", views.add_person, name="add_person"),
+    path("user/<int:person_id>/", views.person_detail, name="person_detail"),
+    path("badge/new/", views.add_badge, name="add_badge"),
+    path("badge/<int:badge_id>/", views.badge_detail, name="badge_detail"),
 ]
--- users/views.py
+++ users/views.py
@@ -14,32 +14,33 @@ def index(request):
     # return HttpResponse(template.render(context, request))
 
 
-def detail(request, person_id):
+def person_detail(request, person_id):
     # try:
     # 	person = Person.objects.get(pk = person_id)
     # except Person.DoesNotExist:
     # 	raise Http404("Person is not in the system.")
     person = get_object_or_404(Person, pk=person_id)
-    return render(request, "users/detail.html", {"person": person})
+    return render(request, "users/user_detail.html", {"person": person})
 
 
-def badge(request, person_id):
-    person = get_object_or_404(Person, pk=person_id)
-    return render(request, "users/badge.html", {"person": person})
+def badge_detail(request, badge_id):
+    badge = get_object_or_404(Badge, pk=badge_id)
+    return render(request, "users/badge_detail.html", {"badge": badge})
 
 
 def add_person(request):
-    try:
-        p_name = request.POST["name"]
-    except (KeyError, Name.DoesNotExist):
-        latest_person_list = Person.objects.order_by("-name")
-        context = {
-            "latest_person_list": latest_person_list,
-            "error_message":      "Invalid Person Name",
-        }
-        return render(request, "users/index.html", context)
+    latest_person_list = Person.objects.order_by("-name")
+    p_name = request.POST.get('name', '')
+    if not p_name:
+        return render(
+            request,
+            'users/index.html',
+            {
+                'error_message': "name field must not be left blank",
+                'latest_person': latest_person_list,
+            },
+        )
     else:
-        latest_person_list = Person.objects.order_by("-name")
         for person in latest_person_list:
             person_name = person.name
             if p_name == person_name:
@@ -55,40 +56,39 @@ def add_person(request):
         return render(request, "users/index.html", context)
 
 
-def add_badge(request, person_id):
+def add_badge(request):
     error = ''
-    person = get_object_or_404(Person, pk=person_id)
 
-    # Get field values or empty string
-    b_name = request.POST.get('name', '')
-    b_presenter = request.POST.get('presenter', '')
+    if request.method == 'POST':
+        # Get field values or empty string
+        b_name = request.POST.get('name', '')
+        b_presenter = request.POST.get('presenter', '')
 
-    # return a warning if any fields were left blank
-    if '' in (b_name, b_presenter):
-        return render(
-            request,
-            'users/badge.html',
-            {
-                'person':
-                    person,
-                'error_message':
-                    'Neither badge name or presenter may be left blank'
-            },
-        )
-    else:
-        badge, _ = Badge.objects.get_or_create(
-            name=b_name,
-            presenter=b_presenter
-        )
-        if badge.user_set.filter(name=person.name):
-            error = f'User has already obtained {badge}'
+        # return a warning if any fields were left blank
+        if '' in (b_name, b_presenter):
+            return render(
+                request,
+                'users/add_badge.html',
+                {
+                    'error_message':
+                        'Neither badge name or presenter may be left blank'
+                },
+            )
         else:
-            badge.user_set.add(person)
+            badge, _ = Badge.objects.get_or_create(
+                name=b_name,
+                presenter=b_presenter
+            )
+            # if badge.user_set.filter(name=person.name):
+            #     error = f'User has already obtained {badge}'
+            # else:
+            #     badge.user_set.add(person)
             badge.save()
 
-        return render(request, "users/detail.html",
-                      {
-                          "person":        person,
-                          "error_message": error
-                      },
-                      )
+    return render(
+        request,
+        "users/add_badge.html",
+        {
+            "error_message": error
+        },
+    )
