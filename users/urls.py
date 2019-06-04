183fb489ecb6f89817bf928a5fe815095bdb6bf4
kyrogon <aaron.kyle.rogers@gmail.com>
2019-06-03 19:13:03 -0600

    brought back original functionality

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("<int:person_id>/", views.detail, name="detail"),
     path("<int:person_id>/badge/", views.badge, name="badge"),
-    path("add_badge/", views.add_badge, name="add_badge"),
+    path("<int:person_id>/add_badge/", views.add_badge, name="add_badge"),
     path("add_person/", views.add_person, name="add_person"),
 ]
